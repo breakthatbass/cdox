f7644efefc1658d610f0747880b6cd27661ea175
breakthatbass <gamache.taylor@gmail.com>
2021-09-25 21:28:11 -0400

    clean up code

--- cdox/cdox.py
+++ cdox/cdox.py
@@ -1,31 +1,23 @@
 import sys
-
 from argparse import ArgumentParser
 
 IN_DOCS = False
 IN_FUNC = False
 
-def open_files(infile, outfile):
-    # create markdown file
-    try:
-        doc = open(outfile, 'a')
-    except:
-        print('error: unable to open ' + outfile)
-        return
-    
-    
+def read_infile_into_list(infile):
+    '''attempt to open the infile and outfile. if opened, return them'''
     try:
         cf = open(infile, 'r')
-    except:
-        print('error: unable to create ' + infile)
-        return
-    
-    in_file = cf.readlines()
+    except FileNotFoundError as fnf_error:
+        print(fnf_error)
+        sys.exit()
 
-    return doc, in_file
+    infile_list = cf.readlines()
+    cf.close()
+    return infile_list
 
 
-def parse(files):
+def parse(file, outfile):
     infos = []
     returns = ''
     prototype = ''
@@ -34,65 +26,55 @@ def parse(files):
     global IN_DOCS
     global IN_FUNC
 
-    doc = files[0]
-    hfile = files[1]
+    with open(outfile, 'w') as doc:
+        for line in file:
+            if IN_DOCS == True:
+                if ' * @info: ' in line:
+                    info = line.replace(' * @info: ', '', 1)
+                    infos.append(info)
+                elif ' * @returns:' in line:
+                    info = line.replace(' * @returns: ', '', 1)
+                    returns = info
+                    IN_DOCS = False
+            elif IN_FUNC == True:
+                    prototype = "```C\n" + line + "```\n"
+                    doc.write(prototype)
+                    for poo in infos:
+                        doc.write('- ' + poo)
+                    doc.write('- **returns** ' + returns)
+                    doc.write(new_func)
+                    infos.clear()
+                    IN_FUNC = False
+                    
+            elif line == '/* *\n':
+                IN_DOCS = True
 
-    for line in hfile:
-        if IN_DOCS == True:
-            if ' * @info: ' in line:
-                info = line.replace(' * @info: ', '', 1)
-                infos.append(info)
-            elif ' * @returns:' in line:
-                info = line.replace(' * @returns: ', '', 1)
-                returns = info
-                IN_DOCS = False
-        elif IN_FUNC == True:
-                prototype = "```C\n" + line + "```\n"
-                doc.write(prototype)
-                for poo in infos:
-                    doc.write('- ' + poo)
-                doc.write('- **returns** ' + returns)
-                doc.write(new_func)
-                infos.clear()
-                IN_FUNC = False
-                
-        elif line == '/* *\n':
-            # we're in an a func doc
-            IN_DOCS = True
+            elif line == ' * */\n':
+                IN_FUNC = True
 
-        elif line == ' * */\n':
-            # we're done with docs
-            # next is a function prototype
-            IN_FUNC = True
-
-        elif '@name:' in line:
-            title = '# ' + line.split(':')[1].lstrip().strip() + ' documentation\n'
-            doc.write(title)
-        elif '@description:' in line:
-            desc = line.split(':')[1].lstrip().strip()
-            doc.write(desc + '  \n#\n')
-        else:
-            continue
+            elif '@name:' in line:
+                title = '# ' + line.split(':')[1].lstrip().strip() + ' documentation\n'
+                doc.write(title)
+            elif '@description:' in line:
+                desc = line.split(':')[1].lstrip().strip()
+                doc.write(desc + '  \n#\n')
+            else:
+                continue
 
 
-
-def main():
-    
-        
+def main():        
     if len(sys.argv) != 3:
         print('usage: cdox infile outfile')
-        sys.exit(1)
+        sys.exit()
 
     infile = sys.argv[1]
     outfile = sys.argv[2]
-    
-    # assuming we have the infile and outfile
-    fps = open_files(infile, outfile)
 
-    parse(fps)
+    infile_list = read_infile_into_list(infile)
+    for i in infile_list:
+        print(i, end='')
 
-    #fps[0].close()
-    #fps[1].close()
+    parse(infile_list, outfile)
 
 if __name__ == '__main__':
     main()
