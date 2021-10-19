b78f18d00499edb3d7df36fdd83b9e426eaff89f
breakthatbass <gamache.taylor@gmail.com>
2021-10-19 13:35:50 -0400

    test CI

--- cdox/__init__.py
+++ cdox/__init__.py
@@ -2,8 +2,8 @@
 cdox
 documentation generator for generating API documentation in markdown format from source code
 '''
-from cdox import Doc
-from __main__ import main
+#from cdox import Doc
+#from __main__ import main
 
 __version__ = '1.0.4'
 __author__ = 'Taylor Gamache'
--- tests/cdox_test.py
+++ tests/cdox_test.py
@@ -4,6 +4,11 @@ import os
 
 from pathlib import Path
 
+import os, sys
+dir_path = os.path.dirname(os.path.realpath(__file__))
+parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
+sys.path.insert(0, parent_dir_path)
+
 try:
     from cdox import Doc
 except ImportError:
