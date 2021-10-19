e6489a70e9c89a8df36ae144dca57f83b1f3c60d
breakthatbass <gamache.taylor@gmail.com>
2021-10-19 13:09:42 -0400

    update __init__

--- cdox/__init__.py
+++ cdox/__init__.py
@@ -2,6 +2,8 @@
 cdox
 documentation generator for generating API documentation in markdown format from source code
 '''
+from cdox import *
+from __main__ import *
 
 __version__ = '1.0.4'
 __author__ = 'Taylor Gamache'
